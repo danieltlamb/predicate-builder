08a2beeb84eba2eda4314f3c3ea0831b48baa310
Dan <system76-lemur@pop-os.localdomain>
2019-12-17 02:57:53 -0800

    Fixing Labels

--- client/src/constants/PredicateOptions.js
+++ client/src/constants/PredicateOptions.js
@@ -3,8 +3,8 @@ export const PredicateOptions = [
   { value: "screen_width", label: "Screen Width", type: "NUMBER" },
   { value: "screen_height", label: "Screen Height", type: "NUMBER" },
   { value: "visits", label: "# of Visits", type: "NUMBER" },
-  { value: "user_first_label", label: "First label", type: "STRING" },
-  { value: "user_last_label", label: "Last label", type: "STRING" },
+  { value: "user_first_name", label: "First Name", type: "STRING" },
+  { value: "user_last_name", label: "Last Name", type: "STRING" },
   { value: "page_response", label: "Page Response time (ms)", type: "NUMBER" },
   { value: "domain", label: "Domain", type: "DOMAIN" },
   { value: "path", label: "Page Path", type: "STRING" }
